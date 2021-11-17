19987b4ebdc51c9b68d5ebce37c731fef868523c
Abinemugh Tennyson <abinemugh_tennyson1@baylor.edu>
2021-11-17 15:49:10 -0600

    second class commit

--- Mainapp/admin.py
+++ Mainapp/admin.py
@@ -3,4 +3,8 @@ from django.contrib import admin
 # Register your models here.
 from .models import Topic
 
+# Register entry 
+from .models import Entry
+
 admin.site.register(Topic)
+admin.site.register(Entry)
--- Mainapp/views.py
+++ Mainapp/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, 'MainApp/index.html') #usually a lot but we are being simple, which would involve us evaluating post and gets
+    
